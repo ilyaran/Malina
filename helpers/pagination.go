222adacb5f83adb007fa89992c064406b70e53e5
ilya aranov <ilyaran@mail.ru>
2017-12-05 01:52:53 +0500

    new malina

--- helpers/pagination.go
+++ helpers/pagination.go
@@ -11,7 +11,18 @@
  * @license		MIT License Copyright (c) 2017 John Aran (Ilyas Toxanbayev)
  */
 
-package helpers
+/**
+ *
+ *
+ *
+ * @author		John Aran (Ilyas Aranzhanovich Toxanbayev)
+ * @version		1.0.0
+ * @based on
+ * @email      		il.aranov@gmail.com
+ * @link
+ * @github      	https://github.com/ilyaran/github.com/ilyaran/Malina
+ * @license		MIT License Copyright (c) 2017 John Aran (Ilyas Toxanbayev)
+ */ package helpers
 
 import "fmt"
 
--- views/news.go
+++ views/news.go
@@ -1,4 +1,15 @@
-package views
+/**
+ *
+ *
+ *
+ * @author		John Aran (Ilyas Aranzhanovich Toxanbayev)
+ * @version		1.0.0
+ * @based on
+ * @email      		il.aranov@gmail.com
+ * @link
+ * @github      	https://github.com/ilyaran/github.com/ilyaran/Malina
+ * @license		MIT License Copyright (c) 2017 John Aran (Ilyas Toxanbayev)
+ */ package views
 
 
 /*
